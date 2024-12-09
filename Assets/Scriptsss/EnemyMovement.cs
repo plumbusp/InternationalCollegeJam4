d4bf6f3b70da28b3a7b5d061837c77b4ed936b93
Ilona <venusflytrapd.d@gmail.com>
2024-12-10 00:00:41 +0300

    Trying to create proper rotation

--- Assets/Scriptsss/EnemyMovement.cs
+++ Assets/Scriptsss/EnemyMovement.cs
@@ -26,8 +26,7 @@ public class EnemyMovement : MonoBehaviour
 
     private WayPoint nextPoint; //The point that transform is currently moving towards
 
-    private float _timePassed;
-    [SerializeField] private float _LerpSpeed;
+    private Vector3 _currentAngle; // used in rotation
 
     private void Start()
     {
@@ -67,13 +66,18 @@ public class EnemyMovement : MonoBehaviour
             //Rotating object to look in the directin of next Pos 
             var direction = transform.position - nextPoint.transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Lerp(Quaternion.AngleAxis(angle, transform.forward), transform.rotation, _timePassed);
-            _timePassed += Time.deltaTime;
+            var newAngle = Quaternion.AngleAxis(angle, transform.forward).eulerAngles;
+
+            _currentAngle = new Vector3(
+                    Mathf.LerpAngle(_currentAngle.x, newAngle.x, Time.deltaTime),
+                    Mathf.LerpAngle(_currentAngle.y, newAngle.y, Time.deltaTime),
+                    Mathf.LerpAngle(_currentAngle.z, newAngle.z, Time.deltaTime));
+
+            transform.eulerAngles = new Vector3(Mathf.LerpAngle(_currentAngle.x, newAngle.x, Time.deltaTime), transform.rotation.y, transform.rotation.z);
         }
         else
         {
             transform.position = nextPoint.transform.position;
-            _timePassed = 0;
             nextPoint = GetNextWayPoint();
             Rotate(nextPoint);
         }
