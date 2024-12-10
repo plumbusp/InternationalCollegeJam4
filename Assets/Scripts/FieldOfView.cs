b4fe82cf55d3d4fcac08328016581fd22ec75e16
Ilona <venusflytrapd.d@gmail.com>
2024-12-10 10:50:03 +0200

    Organized folders, created field of view material

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using CodeMonkey.Utils;
+
+public class FieldOfView : MonoBehaviour {
+
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float viewDistance = 50f;
+    [SerializeField] private float fieldOV = 90f;
+    [SerializeField] private int rayCount = 50;
+
+    private Mesh mesh;
+    private Vector3 origin;
+    private float startingAngle;
+
+
+    private void Start() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        origin = Vector3.zero;
+    }
+
+    private void LateUpdate() {
+        float angle = startingAngle;
+        float angleIncrease = fieldOV / rayCount;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i = 0; i <= rayCount; i++) {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, UtilsClass.GetVectorFromAngle(angle), viewDistance, layerMask);
+            if (raycastHit2D.collider == null) {
+                // No hit
+                vertex = origin + UtilsClass.GetVectorFromAngle(angle) * viewDistance;
+            } else {
+                // Hit object
+                vertex = raycastHit2D.point;
+            }
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0) {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+            angle -= angleIncrease;
+        }
+
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+    }
+
+    public void SetOrigin(Vector3 origin) {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection) {
+        startingAngle = UtilsClass.GetAngleFromVectorFloat(aimDirection) + fieldOV / 2f;
+    }
+
+    public void SetFoV(float fov) {
+        this.fieldOV = fov;
+    }
+
+    public void SetViewDistance(float viewDistance) {
+        this.viewDistance = viewDistance;
+    }
+
+}
